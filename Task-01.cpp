f20fdcb1614c5313d3ac55ece466c5a466a6d22f
Jaya-Rani28 <91416214+Jaya-Rani28@users.noreply.github.com>
2021-10-20 15:14:36 +0530

    Add files via upload

---
+++ Task-01.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int age = 13;
+  int salary = 28000;
+  
+  float area = 64.74;
+  double distance = 45E12;
+  char test1='h';
+  wchar_t test2=L'D';
+  
+  bool cond = false;
+  cout<<"int:"<<age<<endl;
+  cout<<"int:"<<salary<<endl;
+  cout<<"Float"<<area<<endl;
+  cout<<"Double:"<<distance<<endl;
+  cout<<"Char:"<<test1<<endl;
+  cout<<"Wchar:"<<test2<<endl;
+  cout<<"Boolean:"<<cond<<endl;
+  return 0;
+  }
+  
+
